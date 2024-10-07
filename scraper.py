6923abea04d69f1a4d54084073aadb11921be99f
Jack C <jkchin2@buffalo.edu>
2024-10-06 21:01:11 -0400

    I FUCKIN COOKED DUDE

--- scraper.py
+++ scraper.py
@@ -55,21 +55,38 @@ def scrapePerSite(url):
             majorTitle = soup.find('h1', id="acalog-content").text
             majorTitle = majorTitle.replace(" ", "-") #replace spaces with dashes so it can be made as a file
             majorTitle = majorTitle.replace("/", "_")
+            print(f"{majorTitle}")
 
             # Find admission criteria
             admissionCriteria = soup.find('ul')
 
             # Important classes to take
-            #! working on this
-            importantClasses = soup.find_all('div', _class="custom_leftpad_20")
-            print(importantClasses)
-            #courseRequirements = 
+            importantClasses = soup.find('div', class_="custom_leftpad_20")
+            # Find children with class 'custom_leftpad_20' using .children
+            requiredClasses = [child for child in importantClasses.children if hasattr(child, 'class') and 'custom_leftpad_20' in child['class']]
             
             with open(f"classes/{majorTitle}.txt", "w") as file:
                 file.write("Admission Criteria:")
-                
                 for requirement in admissionCriteria.children:
                     file.write(requirement.text)
+
+                file.write("\n")
+                
+                file.write("Important Classes:\n")
+                for classList in requiredClasses:
+                    # Get the importance of the classes; ex. "Prerequisite Courses"
+                    classes = classList.find_all('div', class_="acalog-core")
+                    for importance in classes:
+                        title = importance.find('h3') if importance.find('h3') else importance.find('h4')
+                        file.write(f"\n\n{title.text}")
+
+                        # ACTUALLY get the courses this time
+                        #find if classes exist in this text block
+                        class_ = importance.find("ul")
+                        if class_:
+                            class_ = class_.find_all('li')
+                            for course in class_:
+                                file.write(f"{course.text}\n")
 
         except requests.exceptions.RequestException as e:
             print(f"Error scraping {url}: {e}")
@@ -82,9 +99,8 @@ def getDataForEachMajor():
             # Investigates each link
             scrapePerSite(line.strip())
 
+            #! UB please don't kick me out
             time.sleep(random.randint(1,3))
-
-            #TODO: scrape from each link to get descriptions of each class and stuff
 
 # Get the list of URLs
 get_websites(CATALOGSURL)
